3ccd05037ea095be4ee830d3d73a8f5aaeccdfb1
Elielton <elieltonramos14@gmail.com>
2024-06-19 22:23:47 -0300

    feat(frontend): :art: update page hello
    add animations, add description, add animation name

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -1,36 +1,42 @@
-import { Pacifico, Permanent_Marker } from "next/font/google";
-import Image from "next/image";
+/* eslint-disable @next/next/no-img-element */
+'use client'
+import { Permanent_Marker, Caveat, Lilita_One } from "next/font/google";
+import NavBar from "./navBar";
+import { DotLottieReact } from '@lottiefiles/dotlottie-react';
 
-const pacifico = Permanent_Marker({ subsets: ["latin"], weight: '400' });
+const googleFontMarker = Permanent_Marker({ subsets: ["latin"], weight: '400' });
+const googleFontLilita = Lilita_One({ subsets: ["latin"], weight: '400' });
 
-function Home() {
+function Hello() {
   return (
-    <main className="flex min-w-80">
+    <main className="flex flex-col sm:flex-row min-w-80 bg-slate-blue">
 
-      <Image className="fixed" src="./desktop.svg" width={300} height={300} alt="desktop-ilustration" />
-
-      <div className="bg-light-fade min-h-screen w-1/4" >
-        <Image src="./nome.svg" alt="name" width={300} height={300} />
+      <div className="z-0 bg-light-fade min-h-screen w-2/4 max-w-[330px]" >
+        <img
+          className="ml-10 mt-10"
+          src="https://readme-typing-svg.demolab.com?font=Permanent+Marker&size=50&pause=1000&color=7A69EF&random=false&width=435&height=100&lines=Elielton"
+          alt="Typing SVG"
+        />
+        <DotLottieReact
+          className="mr-[-180px]"
+          src="./AnimationDesktop.lottie"
+          loop
+          autoplay
+          width={800}
+          height={800}
+        />
       </div>
 
-      <div className="bg-slate-blue min-h-screen w-3/4">
-
-        <nav>
-          <button>Home</button>
-          <button>About</button>
-          <button>Projects</button>
-          <button>Contact</button>
-        </nav>
-
-        <section>
-          <p>👋 Hi, I am Elielton</p>
-          <p>I am a software engineer and I love to build things.</p>
+      <div className="min-h-screen w-1/2 flex flex-col itens-center justify-around ml-[25%]">
+        <section className="w-[85%]">
+          <NavBar />
+          <p className={`${googleFontMarker.className} rotate-[-3deg] pb-6 text-4xl`}>👋 Olá, eu me chamo Elielton</p>
+          <p className={`${googleFontLilita.className} text-5xl`}>Sou Desenvolvedor Web Full Stack apaixonado por tecnologia, sempre à procura de oportunidades para enfrentar novos desafios e melhorar minhas habilidades em backend e frontend.</p>
         </section>
-
       </div>
 
     </main>
   )
 }
 
-export default Home;
+export default Hello;
---
+++ src/components/navBar.tsx
@@ -0,0 +1,12 @@
+function NavBar() {
+  return (
+    <nav className="absolute top-0 mt-10">
+      <button>Home</button>
+      <button>About</button>
+      <button>Projects</button>
+      <button>Contact</button>
+    </nav>
+  )
+}
+
+export default NavBar;
