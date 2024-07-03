896111caa848b493f1f739f2bd8e349d4f856102
Elielton <elieltonramos14@gmail.com>
2024-07-03 16:29:41 -0300

    refactor: remove unused SVG files and update page layout

--- src/components/hello.tsx
+++ src/components/hello.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable @next/next/no-img-element */
+
 'use client'
 import { Permanent_Marker, Caveat, Lilita_One } from "next/font/google";
 import NavBar from "./navBar";
@@ -19,7 +20,7 @@ function Hello() {
         />
         <DotLottieReact
           className="mr-180n w-48"
-          src="./AnimationDesktop.lottie"
+          src="./animations/desktop.lottie"
           loop
           autoplay
           width={800}
@@ -29,20 +30,66 @@ function Hello() {
 
       <NavBar />
       <div className="min-h-screen w-3/5 flex flex-col itens-center mt-20 ml-10">
-          <p className={`${googleFontMarker.className} rotate-[-3deg] py-3 md:py-8 text-responsive`}>👋 Olá, eu me chamo Elielton</p>
-          <p className={`${googleFontLilita.className} text-responsive`}>Sou Desenvolvedor Web Full Stack apaixonado por tecnologia.
-          <br/>
-          Estou sempre à procura de oportunidades, formado na Trybe, tenho conhecimentos em JavaScript, TypeScript, Kotlin e Python</p>
+        <p className={`${googleFontMarker.className} rotate-[-3deg] py-3 md:py-8 text-responsive`}>👋 Olá, eu me chamo Elielton</p>
+        <p className={`${googleFontLilita.className} text-responsive`}>Sou Desenvolvedor Web Full Stack apaixonado por tecnologia.
+          <br />
+          Estou sempre à procura de oportunidades, formado na Trybe, tenho conhecimentos em JavaScript, TypeScript, Kotlin e Python
+        </p>
+        <div className="flex">
+          <DotLottieReact
+            className="w-28"
+            src="./animations/javascript.json"
+            loop
+            autoplay
+            width={800}
+            height={800}
+          />
+          <DotLottieReact
+            className="w-28"
+            src="./animations/kotlin.lottie"
+            loop
+            autoplay
+            width={800}
+            height={800}
+          />
+          <DotLottieReact
+            className="w-28"
+            src="./animations/python.lottie"
+            loop
+            autoplay
+            width={800}
+            height={800}
+          />
+          <DotLottieReact
+            className="w-24"
+            src="./animations/react.lottie"
+            loop
+            autoplay
+            width={800}
+            height={800}
+          />
+          <DotLottieReact
+            className="w-24"
+            src="./animations/docker.lottie"
+            loop
+            autoplay
+            width={800}
+            height={800}
+          />
+        </div>
+        <div className="flex">
+          <img
+            className="w-20 h-20 mt-3"
+            src="./animations/typescript.gif"
+            alt="typescript"
+          />
+          <img
+            className="h-28"
+            src="./linux.png"
+            alt="linux"
+          />
+        </div>
       </div>
-
-      <DotLottieReact 
-        className="w-48"
-        src="./icons8-javascript.json"
-        loop
-        autoplay
-        width={800}
-        height={800}
-      />
 
     </main>
   )
