d2a123123861b40be57b498260558291935f9cab
Elielton <elieltonramos14@gmail.com>
2024-07-08 18:18:23 -0300

    refactor: Add CardProject component and update Projects page layout

--- src/pages/projects.tsx
+++ src/pages/projects.tsx
@@ -4,8 +4,15 @@ function Projects() {
   return (
     <main className="min-h-screen min-w-screen bg-lilac">
       <h1 className="text-4xl font-bold">Projetos</h1>
-      <section className="flex flex-col items-center justify-center">
-        <CardProject />
+      <section className="flex items-center justify-center">
+        <CardProject
+          name="Login Social"
+          description="Uma plataforma onde e possivel fazer posts"
+          typeProject="Project Fullstack"
+          image="/loginSocial.png"
+          repo="https://github.com/ElieltonRamos/project-fullstack-login-social"
+          deploy="login-social-one.vercel.app/"
+        />
       </section>
     </main>
   )
