bc065ec15e0e15601eac94f8ba970419e688174b
Elielton <elieltonramos14@gmail.com>
2025-08-05 21:23:14 -0300

    feat: :construction: criando pagina sobre

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from "./pages/home/home";
+import { About } from './pages/about/about';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home, About],
   templateUrl: './app.html',
 })
 export class App {
