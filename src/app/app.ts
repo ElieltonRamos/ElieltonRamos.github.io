46b28898c5ee9dcc27377bf037b32df8a907d8e7
Elielton <elieltonramos14@gmail.com>
2025-08-06 10:22:52 -0300

    feat: :construction: criando pagina de contato

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Home } from "./pages/home/home";
 import { About } from './pages/about/about';
 import { Formation } from "./pages/formation/formation";
 import { Projects } from './pages/projects/projects';
+import { Contact } from "./pages/contact/contact";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, About, Formation, Projects],
+  imports: [RouterOutlet, Home, About, Formation, Projects, Contact],
   templateUrl: './app.html',
 })
 export class App {
