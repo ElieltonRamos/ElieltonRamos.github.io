c0feb4380e8f81bc8ca58fd5f2dbe2150006ffa9
Elielton <elieltonramos14@gmail.com>
2025-08-06 08:31:59 -0300

    feat: :construction: criando pagina de formaçoes

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Home } from "./pages/home/home";
 import { About } from './pages/about/about';
+import { Formation } from "./pages/formation/formation";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Home, About],
+  imports: [RouterOutlet, Home, About, Formation],
   templateUrl: './app.html',
 })
 export class App {
